6956cf6a2fa0e249a7078bdb248979256be3288b
Matheus Kaúlly <kaully.santos@gmail.com>
2024-03-28 18:38:38 -0300

    add player context

---
+++ src/contexts/PlayerContext.tsx
@@ -0,0 +1,57 @@
+'use client'
+
+import { ReactNode, createContext, useState } from 'react'
+
+interface Episode {
+  title: string
+  members: string
+  thumbnail: string
+  duration: number
+  url: string
+}
+
+interface PlayerContextData {
+  episodeList: Episode[]
+  currentEpisodeIndex: number
+  isPlaying: boolean
+  play: (episode: Episode) => void
+  togglePlay: () => void
+  setIsPlayingState: (state: boolean) => void
+}
+
+export const PlayerContext = createContext({} as PlayerContextData)
+
+export default function PlayerProvider({ children }: { children: ReactNode }) {
+  const [episodeList, setEpisodeList] = useState<Episode[]>([])
+  const [currentEpisodeIndex, setCurrentEpisodeIndex] = useState(0)
+  const [isPlaying, setIsPlaying] = useState(false)
+
+  function play(episode: Episode) {
+    setEpisodeList([episode])
+    setCurrentEpisodeIndex(0)
+    setIsPlaying(true)
+  }
+
+  function togglePlay() {
+    setIsPlaying(!isPlaying)
+  }
+
+  function setIsPlayingState(state: boolean) {
+    setIsPlaying(state)
+  }
+
+  return (
+    <PlayerContext.Provider
+      value={{
+        episodeList,
+        currentEpisodeIndex,
+        play,
+        isPlaying,
+        togglePlay,
+        setIsPlayingState,
+      }}
+    >
+      {children}
+    </PlayerContext.Provider>
+  )
+}
